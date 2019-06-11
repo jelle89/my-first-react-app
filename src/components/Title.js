402aaaf581036765e8dbdee77c0ed7cb9e18b879
Jelle Monen <jelle.monen@hotmail.com>
2019-06-11 12:02:03 +0200

    added lightswitch

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 
 export default class Title extends Component {
   render() {
-    return 'test'
+    
+    return ( <main>
+      <Title content="Some Simple Title" />
+    </main>)
   }
 }
